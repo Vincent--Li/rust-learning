9afc83897b5df86af486201396990a8a0b2122c8
Vincent Li <q315744068@gmail.com>
2025-03-29 11:51:37 +0800

    'static explain in details

---
+++ examples/static_template_example.rs
@@ -0,0 +1,40 @@
+use std::fmt::Display;
+
+fn main() {
+  let r1;
+  let r2;
+  {
+    static STATIC_EXAMPLE: i32 = 42;
+    r1 = &STATIC_EXAMPLE;
+    let x = "&'static str";
+    r2 = x;
+    // r1 和 r2 持有的数据都是 'static 的，因此在花括号结束后，并不会被释放
+  }
+
+  println!("&'static i32: {}", r1); // -> 42
+  println!("&'static str: {}", r2); // -> &'static str
+
+  let r3: &str;
+
+  {
+    let s1 = "String".to_string();
+
+    // s1 虽然没有 'static 生命周期，但是它依然可以满足 T: 'static 的约束
+    // 充分说明这个约束是多么的弱。。
+    // 此时 T = String
+    static_bound(&s1);
+
+    // s1 是 String 类型，没有 'static 的生命周期，因此下面代码会报错
+    r3 = &s1;
+
+    // s1 在这里被 drop
+  }
+  println!("{}", r3);
+}
+
+// T: 'static 约束, 意味着两种情况:
+// 1. T 是一个引用类型，并且它的生命周期是'static
+// 2. T 是一个非引用类型, 默认就满足 'static
+fn static_bound<T: Display + 'static>(t: &T) {
+  println!("{}", t);
+}
---
+++ examples/static_template_example1.rs
@@ -0,0 +1,18 @@
+use std::fmt::Debug;
+
+fn print_it<T: Debug + 'static>( input: T) {
+    println!( "'static value passed in is: {:?}", input );
+}
+
+fn print_it1( input: impl Debug + 'static ) {
+    println!( "'static value passed in is: {:?}", input );
+}
+
+
+
+fn main() {
+    let i = 5;
+    // 此时 T = &i32
+    print_it(&i);
+    print_it1(&i);
+}
